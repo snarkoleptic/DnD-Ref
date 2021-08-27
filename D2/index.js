d6d1d7eb2e36b8a712e904243847566e03447f60
Bryan Salsieder <aegisofsnow@gmail.com>
2021-08-26 19:55:07 -0500

    Trying it from scratch again.

---
+++ D2/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const output = (
+    <div>
+        <p>Hello, world!</p>
+    </div>
+);
+
+
+ReactDOM.render({output},document.getElementById('root'));
